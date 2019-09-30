5f4acd0f5b99a1b8adec9c703a34eb1e8c65d11f
Сергей Филиппов <sfilippov@kp.ru>
2019-09-30 20:42:27 +0300

    update

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# esp8266-vanna
+
+Before building you need copy config-example.h to config.h and edit it.
---
+++ src/config-example.h
@@ -0,0 +1,4 @@
+/* example configuration */
+
+#define WIFI_SSID "insert ssid wifi network";
+#define WIFI_PASS "insert password wifi network"
